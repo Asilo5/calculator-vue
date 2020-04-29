b44abb0112eea613e419fd4ea56072b72b8bafd5
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-04-29 10:54:51 -0600

    Add grid styling to section of calculator

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,6 +1,16 @@
 <template>
-  <section  class="hello">
-     <h1>Hello World</h1>
+  <section  class="calculator">
+      <div>8</div>
+      <div>7</div>
+      <div>6</div>
+      <div>5</div>
+      <div>4</div>
+      <div>3</div>
+      <div>2</div>
+      <div>1</div>
+      <div>=</div>
+      <div>.</div>
+      <div>0</div>
   </section >
 </template>
 
@@ -12,5 +22,9 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+  .calculator {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-auto-rows: minmax(50px, auto);
+  }
 </style>
