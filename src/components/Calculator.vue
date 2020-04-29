42672cd70bab6c17755237e0da5cbd2267366581
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-04-29 11:05:44 -0600

    Add all necessary elements for the calculator

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,16 +1,25 @@
 <template>
   <section class="calculator">
       <div class="display">8njkfdnvjkdnvj</div>
+      <div>AC</div>
+      <div>+/-</div>
+      <div>%</div>
+      <div>÷</div>
       <div>7</div>
-      <div>6</div>
-      <div>5</div>
+      <div>8</div>
+      <div>9</div>
+      <div>x</div>
       <div>4</div>
-      <div>3</div>
-      <div>2</div>
+      <div>5</div>
+      <div>6</div>
+      <div>-</div>
       <div>1</div>
-      <div>=</div>
-      <div>.</div>
+      <div>2</div>
+      <div>3</div>
+      <div>+</div>
       <div>0</div>
+      <div>.</div>
+      <div>=</div>
   </section >
 </template>
 
